89b145bb89bc115eec5ec1b4e78c35fdab05cd68
zhouwei1997 <hengyu19971023@163.com>
2022-04-22 14:23:19 +0800

    聊天

---
+++ 聊天.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+"""
+# @author ZhouWei
+# @date  2022/4/22
+# @file  聊天.py
+# description 聊天机器人互相聊天
+"""
+from time import sleep
+
+import requests
+
+s = input("请主人输入话题：")
+while True:
+    resp = requests.post(
+        "http://www.tuling123.com/openapi/api",
+        data={
+            "key": "4fede3c4384846b9a7d0456a5e1e2943",
+            "info": s,
+        })
+    resp = resp.json()
+    sleep(1)
+    print("小鱼：", resp['text'])
+    s = resp['text']
+    resp = requests.get("http://api.qingyunke.com/api.php",
+                        {'key': 'free', 'appid': 0, 'msg': s})
+    resp.encoding = 'utf8'
+    resp = resp.json()
+    sleep(1)
+    print("菲菲：", resp["content"])
